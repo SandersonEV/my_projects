39ae03f1cc990f6955aa871f2e244476458b551d
SandersonEV <102122962+SandersonEV@users.noreply.github.com>
2022-11-02 08:27:18 -0300

    Building a schema for a restaurant booking
    Building a database schema for a restaurant booking system

---
+++ Meta - Database engineer/1 - Intruduction to Database/1 - schema.sql
@@ -0,0 +1,133 @@
+-- This database is an exercise from Meta Database Enginner Course - 1 Module, Week 4 
+
+-- Obs1: The table and the primary key of the table must be created before of the assignment of the foreign key in another table.
+-- Tip1: Firs create the tables that don't have any foreign keys.
+
+DROP DATABASE IF EXISTS meta_restaurant;
+
+CREATE DATABASE meta_restaurant;
+
+CREATE TABLE tbl( 
+
+    table_id INT PRIMARY KEY, /* I can define the primary key when i'm assigning the data type of table_id */
+
+    location VARCHAR(255) 
+
+); 
+
+CREATE TABLE menu( 
+
+    menu_id INT, 
+
+    description VARCHAR(255), 
+
+    availability INT, 
+
+    PRIMARY KEY (menu_id) 
+
+); 
+
+CREATE TABLE waiter( 
+
+    waiter_id INT, 
+
+    name VARCHAR(150), 
+
+    contact_no VARCHAR(10), 
+
+    shift VARCHAR(10), 
+
+    PRIMARY KEY (waiter_id) /* Or i assign the primary key after everything */
+
+); 
+
+CREATE TABLE table_order( 
+
+    order_id INT, 
+
+    date_time DATETIME, 
+
+    table_id INT, 
+
+    waiter_id INT, 
+
+    PRIMARY KEY (order_id), 
+
+    FOREIGN KEY (table_id) REFERENCES tbl(table_id), /* The foreign key (table_id) in this table references the primary key (table_id) in the tbl table */
+
+    FOREIGN KEY (waiter_id) REFERENCES waiter(waiter_id) /* The foreign key (waiter_id) in this table references the primary key (waiter_id) in the waiter table */
+
+); 
+
+CREATE TABLE customer( 
+
+    customer_id INT, 
+
+    name VARCHAR(100), 
+
+    NIC_no VARCHAR(12), 
+
+    contact_no VARCHAR(10), 
+
+    PRIMARY KEY (customer_id) 
+
+); 
+
+CREATE TABLE reservation( 
+
+    reservation_id INT, 
+
+    date_time DATETIME, 
+
+    no_of_pax INT, 
+
+    order_id INT, 
+
+    table_id INT, 
+
+    customer_id INT, 
+
+    PRIMARY KEY (reservation_id), 
+
+    FOREIGN KEY (order_id) REFERENCES table_order(table_id), 
+
+    FOREIGN KEY (table_id) REFERENCES tbl(table_id), 
+
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id) 
+
+); 
+
+CREATE TABLE menu_item( 
+
+    menu_item_id INT, 
+
+    description VARCHAR(255), 
+
+    price FLOAT, 
+
+    availability INT, 
+
+    menu_id INT, 
+
+    PRIMARY KEY (menu_item_id), 
+
+    FOREIGN KEY (menu_id) REFERENCES menu(menu_id) 
+
+); 
+
+CREATE TABLE order_menu_item( 
+
+    order_id INT, 
+
+    menu_item_id INT, 
+
+    quantity INT, 
+
+    PRIMARY KEY (order_id,menu_item_id), 
+
+    FOREIGN KEY (order_id) REFERENCES table_order(order_id), 
+
+    FOREIGN KEY (menu_item_id) REFERENCES menu_item(menu_item_id) 
+
+); 
+
