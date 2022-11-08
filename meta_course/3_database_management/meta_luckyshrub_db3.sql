930e90942c1b1a8ad9d383d72e40f28f61885b1a
SandersonEV <102122962+SandersonEV@users.noreply.github.com>
2022-11-08 08:12:31 -0300

    Update meta_luckyshrub_db3.sql

--- meta_course/3_database_management/meta_luckyshrub_db3.sql
+++ meta_course/3_database_management/meta_luckyshrub_db3.sql
@@ -85,10 +85,15 @@ VALUES(1,3,"In Progress",200),
 (14,4,"Not Recieved",250); 
 
 -- Task1: Use the ANY operator to identify all employees with the Order Status status 'Completed'. 
+/* 
+The ANY operator returns a boolean value following a comparison operation. It returns a TRUE value if ANY subquery values meet the given condition. 
+In other words, the condition will be TRUE if the operation is true for any of the values in the range.
+*/
 
 SELECT EmployeeID ID, EmployeeName Nome
 FROM employees
-WHERE EmployeeID = ANY (SELECT EmployeeID FROM employee_orders WHERE Status = "Completed" ); /* This way is not very good becouse you can only check which EmployeeID is in the list of the subquery in the where clause */
+WHERE EmployeeID = ANY (SELECT EmployeeID FROM employee_orders WHERE Status = "Completed" ); /* Verify if exists any matches between the results of the subquery that uses the employee_orders table
+in the EmployeeID from the employees table. Subqueries works like a Join in some cases but i don't need keys */
 
 -- Other three optins to complete the Task1:
 SELECT EmployeeID ID, EmployeeName Nome
@@ -100,7 +105,7 @@ FROM employees e
 INNER JOIN employee_orders eo ON e.EmployeeID = eo.EmployeeID
 WHERE eo.Status = "Completed"
 GROUP BY e.EmployeeName
-ORDER BY COUNT(e.EmployeeName) DESC; /* Use i INNER JOIN look to be the best way because you can also extract other data from the employee_orders table */
+ORDER BY COUNT(e.EmployeeName) DESC;
 
 SELECT e.EmployeeID ID, e.EmployeeName 'Nome do Funcionário', COUNT(e.EmployeeName) 'Nº de Pedidos Completos'
 FROM employees e
@@ -110,35 +115,57 @@ GROUP BY e.EmployeeName
 ORDER BY COUNT(e.EmployeeName) DESC; /* You can also make this task using the RIGHT JOIN because he asks to everyone with the status completed in the right table (this option looks more correct for me) */
 
 -- Task 2: Use the ALL operator to identify the IDs of employees who earned a handling cost of "more than 20% of the order value" from all orders they have handled.
-
-SELECT EmployeeID
+/* 
+Use the ALL operator for the same purpose as the ANY operator. However, the way it works is a little bit different. It returns a boolean value as a result of performing a comparison operation.
+ It returns TRUE only if ALL subquery values meet the given condition. In other words, the condition will be TRUE only if the operation is true for all values in the range. 
+*/
+SELECT DISTINCT EmployeeID Identificação
 FROM employee_orders
-WHERE HandlingCost > ALL(SELECT ROUND(o.OrderTotal/100 * 20) FROM orders o); 
+WHERE HandlingCost > ALL(SELECT ROUND(o.OrderTotal* 0.2) FROM orders o); 
 
-SELECT e.EmployeeID ID, e.EmployeeName 'Nome do Funcionário'
-FROM employees e
-WHERE e.EmployeeID IN (
-SELECT EmployeeID
-FROM employee_orders
-WHERE HandlingCost > ALL(SELECT ROUND(o.OrderTotal/100 * 20) FROM orders o)
-);
-
-
-SELECT EmployeeID ID, EmployeeName 'Nome do Funcionário'
+-- Other ways to complete this task:
+SELECT EmployeeID Identificaçao, EmployeeName Nome, Department Departamento
 FROM employees
-WHERE EmployeeID IN (
-SELECT EmployeeID
+WHERE
+EmployeeID IN (SELECT DISTINCT EmployeeID Identificação
+FROM employee_orders
+WHERE HandlingCost > ALL(SELECT ROUND(o.OrderTotal* 0.2) FROM orders o)
+); 
+
+-- In this question I understood that I should calculate the SUM() and not every item separeted. So i did something more difficut and i'll keep beacause is a good query to learn
+
+/* It's a subquery with a LEFT JOIN - the propouse is of this query is too see if the SUM(n.HandlingCost) from the employees_orders table is greater 
+than the SUM(n.OrderTotal)*0.2 from the orders table
+*/
+SELECT n.EmployeeID ID, 
+CASE WHEN SUM(n.OrderTotal)*0.2 > SUM(n.HandlingCost) = 1 THEN 'Yes' ELSE 'No' END AS 'Foi maior?'
+FROM (
+SELECT eo.EmployeeID, o.OrderTotal,eo.HandlingCost
 FROM employee_orders eo
-INNER JOIN orders o ON eo.OrderID = o.OrderID
-WHERE eo.HandlingCost >= (o.OrderTotal)*0.2
-GROUP BY eo.EmployeeID
-);
+LEFT JOIN orders o ON eo.OrderID = o.OrderID
+) AS n
+GROUP BY n.EmployeeID
+HAVING SUM(n.HandlingCost) > SUM(n.OrderTotal)*0.2;
 
-select sum(HandlingCost) from employee_orders group by EmployeeID; 
-select SUM(OrderTotal) FROM orders group by OrderID;
-
+-- 1 - in the subquery with the JOIN I create another table 'n' with the important data to answer the task. 
+-- 2 - in the out query I grouped usind the ID as the parameter and maked the sum of the others
+-- 3 - Than I've use a filter (HAVING - when the filter has agregation function) to filter using the requested filter in the task
+-- Extra: Basically, you use CASE statment to convert from bit answer (1 or 0) to the values yes or no.
+-- (Obs: This query don't answer exactly the task because he makes the SUM() of all HandlingCost and OrderTotal of each specified EmployeeID and make the calculation of the Total)
 
 -- Task 3: Use the GROUP BY clause to summarize the duplicate records with the same column values into a single record by grouping them based on those columns.
 
+/*
+SELECT EmployeeID,HandlingCost   
+FROM employee_orders   
+WHERE HandlingCost > ALL(SELECT ROUND(OrderTotal/100 * 20) FROM orders) GROUP BY EmployeeID,HandlingCost;
+*/
 
 -- Task 4: Use the HAVING clause to filter the grouped data in the subquery that you wrote in task 3 to filter the 20% OrderTotal values to only retrieve values that are more than $100.
+
+/*
+SELECT EmployeeID,HandlingCost 
+FROM employee_orders  
+WHERE HandlingCost > ALL(SELECT ROUND(OrderTotal/100 * 20) AS twentyPercent FROM orders  GROUP BY OrderTotal  
+HAVING twentyPercent > 100)  GROUP BY EmployeeID,HandlingCost;     
+*/
