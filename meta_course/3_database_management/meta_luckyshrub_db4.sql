4aad5123941f4be808f990c3faee74618827e39e
SandersonEV <102122962+SandersonEV@users.noreply.github.com>
2022-11-10 13:20:03 -0300

    Update meta_luckyshrub_db4.sql
    Add some Procedures

--- meta_course/3_database_management/meta_luckyshrub_db4.sql
+++ meta_course/3_database_management/meta_luckyshrub_db4.sql
@@ -66,3 +66,29 @@ Task 4: Write a SQL statement to delete the Orders virtual table.
 */
 
 DROP VIEW ClientsOrdersView2;
+
+-- ------------------------------------------------------------------------------------------- Procedures ---------------------------------------------------------------------------------------
+
+/*
+Task 1: Write a SQL statement that creates a stored procedure called 'GetOrdersData' which retrieves all data from the Orders table.
+After executing the query, call the “GetOrdersData” to show all orders on the MySQL terminal.
+*/
+CREATE PROCEDURE GetOrdersData()
+SELECT * 
+FROM orders;
+
+CALL GetOrdersData(); -- test
+
+/*
+Task 2: Write a SQL statement that creates a stored procedure called “GetListOfOrdersInRange”. The procedure must contain two parameters that determine the range of retrieved data based on the user input of 
+two cost values “MinimumValue” and “MaximumValue”.
+Once you have executed the query, call the “GetListOfOrdersInRange” to display the data of orders that cost between $150 and $600. 
+*/
+
+CREATE PROCEDURE GetListOfOrdersInRange(MinimumValue INT,MaximumValue INT)
+SELECT * 
+FROM orders 
+WHERE COST BETWEEN MinimumValue AND MaximumValue;
+
+CALL GetListOfOrdersInRange(150, 600); -- test
+
