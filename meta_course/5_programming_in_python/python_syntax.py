8cd369eb520e84643414420ac2809362a8e1d210
SandersonEV <102122962+SandersonEV@users.noreply.github.com>
2022-11-17 22:45:19 -0300

    Basic python

---
+++ meta_course/5_programming_in_python/python_syntax.py
@@ -0,0 +1,23 @@
+name = 'John'
+if name == 'John': # The if statement and other functions need the identation to work
+    name = 'San'    
+    print(name)
+
+a = b = c = d = 10 # This means that the avriable a, b, c, d are iqual to 10
+a, b, c, d = 1,3,5,7 # a=1 b=3 c=5 and d=7
+
+########## Data types #################
+#  NUMERIC 
+a = 1 ; b  = -1 #int
+a = 1.1 ; b = -1.1 #float
+a = 10 + 10j # complex number
+# SEQUENCE
+a = 'Sand' #str
+a = [1, 'say', 23, 4.2, 10+ 10j] #list
+a = (1, 'say', 23, 4.2, 10+ 10j) #Tuples = Are like the list but can not be modified
+# BOOLEAN
+a = True ; b = False
+# DICTIONARY
+a = {'a': 10,'b': -4.5,'c':'sarf'} #Dictionary = Every key has one value. Ex: a['c'] = 'sarf'
+# SET
+a =  {1, 'say', 23, 4.2, 10+ 10j} # No ordered, No Index but No duplicate Elements (is good to remove duplicates from a list)
